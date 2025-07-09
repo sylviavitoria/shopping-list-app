9a4ea0f4f401c22f51a0108f8a39b7653eebb316
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-07-09 15:36:15 -0300

    chore: atualizar configuração do Jest para incluir novos padrões de teste e cobertura

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,28 @@ module.exports = {
     'node_modules/(?!(jest-)?react-native|@react-native|expo|@expo|react-navigation|@react-navigation|native-base|react-native-svg|firebase)',
   ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/coverage/',
+    '<rootDir>/assets/',
+    '<rootDir>/.expo/',
+  ],
+  
   collectCoverageFrom: [
-    '**/*.{ts,tsx}',
-    '!**/coverage/**',
-    '!**/node_modules/**',
-    '!**/babel.config.js',
-    '!**/jest.setup.js',
+    'src/components/**/*.{ts,tsx}',
+    'src/hooks/**/*.{ts,tsx}',
+    'src/pages/**/*.{ts,tsx}',
+    'src/service/**/*.{ts,tsx}',
+
+    '!src/service/config/firebase.ts',        
+    '!src/models/**',                         
+    '!src/app/_layout.tsx',                  
+    '!src/app/index.tsx',                  
+    '!**/__tests__/**',                    
+    '!**/*.test.{ts,tsx}',                    
+    '!**/coverage/**',                        
+    '!**/node_modules/**',                   
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleNameMapper: {
